dd4a02b3142103cdfa3d5b67b7491948a4459508
Prakash8782 <prakash8782@gmail.com>
2022-09-19 14:40:00 +0530

    foreach

---
+++ foreach/main.js
@@ -0,0 +1,10 @@
+let colors=["Red","Blue","Green","Yellow","Pink"];
+function color(x){
+    console.log(x);
+}
+console.log("colors : " ,colors);
+console.log("length of an array : "+colors.length);
+console.log("method : array.forEach()");
+console.log("change in array : ",);
+colors.forEach(color);
+console.log("return : length of array after forEach(); is "+colors.length);
